9309ef5ffbb36db2cbf30c209406e89c335bfb2a
Bradley Grainger <bgrainger@gmail.com>
2013-05-14 06:33:22 -0700

    Add new C# project.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CoverityBugs")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CoverityBugs")]
+[assembly: AssemblyCopyright("Copyright 2013 Bradley Grainger")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
