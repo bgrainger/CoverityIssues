6fc57e96fda18aff8729d183e03ed73c6779a263
Bradley Grainger <bgrainger@gmail.com>
2013-05-14 06:43:55 -0700

    Add equivalent files with different line counts.

---
+++ LineCount1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CoverityIssues
+{
+	class LineCount1
+	{
+		public void Method(int value) {
+			if (value < 0) {
+				Console.WriteLine("negative");
+			} else if (value == 0) {
+				Console.WriteLine("positive");
+			} else {
+				Console.WriteLine("zero");
+			}
+		}
+	}
+}
