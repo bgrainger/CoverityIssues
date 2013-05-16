a42fd3ee02cad2eb7415bd13e493c6382ad44ba8
Bradley Grainger <bgrainger@gmail.com>
2013-05-16 16:29:36 -0700

    Add DisposableStructResourceLeak example.

---
+++ DisposableStructResourceLeak.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace CoverityIssues
+{
+	public static class DisposableStructResourceLeak
+	{
+		public static void Method()
+		{
+			using (DisposableStruct ds = new DisposableStruct(IntPtr.Zero))
+			{
+			}
+		}
+
+		private struct DisposableStruct : IDisposable
+		{
+			public DisposableStruct(IntPtr ptr)
+			{
+				m_ptr = ptr;
+			}
+
+			public void Dispose()
+			{
+				if (m_ptr != IntPtr.Zero)
+				{
+					Marshal.FreeCoTaskMem(m_ptr);
+					m_ptr = IntPtr.Zero;
+				}
+			}
+
+			IntPtr m_ptr;
+		}
+	}
+}
