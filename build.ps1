0bfe9ebbadfd547ce738dde85d733060662f2227
Bradley Grainger <bgrainger@gmail.com>
2013-09-03 14:12:09 -0700

    Use new version of Coverity.

--- build.ps1
+++ build.ps1
@@ -1,17 +1,20 @@
 Task Default -depends Coverity
 
 Task Build {
-  Exec { msbuild /m:4 /property:Configuration=Release CoverityIssues.sln }
+  mkdir build\coverity -force
+  & "C:\Program Files\Coverity\cov-analysis-win64-fresno-cs-beta\bin\cov-build.exe" `--dir build\coverity `--debug `--enable-cs-parse-error-recovery msbuild /m:4 /property:Configuration=Release CoverityIssues.sln
+  if ($LastExitCode -ne 0) {
+    throw "Error executing cov-build.exe"
+  }
 }
 
 Task Coverity -depends Build {
-  mkdir build\coverity -force
-  & "C:\Program Files\Coverity\Coverity Static Analysis\bin\cov-analyze-cs.exe" `--all `--max-mem 8192 `--dir build\coverity bin\Release\CoverityIssues.exe
+  & "C:\Program Files\Coverity\cov-analysis-win64-fresno-cs-beta\bin\cov-analyze-cs.exe" `--all `--dir build\coverity -j auto `--strip-path $(get-location).Path
   if ($LastExitCode -ne 0) {
     throw "Error executing cov-analyze-cs.exe"
   }
   $headSha = & "C:\Program Files (x86)\Git\bin\git.exe" rev-parse HEAD
-  & "C:\Program Files\Coverity\Coverity Static Analysis\bin\cov-commit-defects.exe" `--host coverity `--dataport 9090 `--stream CoverityIssues `--user logosutilityreporter `--password password `--dir build\coverity `--version $headSha `--strip-path $(get-location).Path
+  & "C:\Program Files\Coverity\cov-analysis-win64-fresno-cs-beta\bin\cov-commit-defects.exe" `--host coverity `--dataport 9090 `--stream CoverityIssuesBeta `--user logosutilityreporter `--password password `--dir build\coverity `--version $headSha
   if ($LastExitCode -ne 0) {
     throw "Error executing cov-commit-defects.exe"
   }
