a21a8aa1bb40c464bf46b3e13507c5cb04c526d9
Bradley Grainger <bgrainger@gmail.com>
2013-09-04 13:52:42 -0700

    Add FORWARD_NULL repro from generic interface.

---
+++ GenericInterface.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace CoverityIssues
+{
+	public static class GenericInterface
+	{
+		public static void TestMethod()
+		{
+			// Explicit null dereferenced (FORWARD_NULL) / var_deref_model: Passing "null" to "Execute", which dereferences it. (The virtual call resolves to "CoverityIssues.Behavior`1::Execute(System.Object)".)
+			MyBehavior.Execute(null);
+		}
+
+		// Changing "IBehavior" to "Behavior" prevents the issue being detected.
+		private static IBehavior MyBehavior
+		{
+			get
+			{
+				return new Behavior(() => Console.WriteLine("test"));
+			}
+		}
+	}
+
+	public interface IBehavior
+	{
+		void Execute(object param);
+	}
+
+	public class Behavior : IBehavior
+	{
+		public Behavior(Action action)
+		{
+			m_action = action;
+		}
+
+		public void Execute(object param)
+		{
+			m_action();
+		}
+
+		readonly Action m_action;
+	}
+
+	public class Behavior<T> : IBehavior
+	{
+		public Behavior(Action<T> action)
+		{
+			m_action = action;
+		}
+
+		public void Execute(object param)
+		{
+			m_action((T) param);
+		}
+
+		readonly Action<T> m_action;
+	}
+}
