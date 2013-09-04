c4894e137033e358aab44b92e11db8d7629663a4
Bradley Grainger <bgrainger@gmail.com>
2013-09-04 13:46:58 -0700

    Try to repro CID 10341 (FORWARD_NULL with Interlocked.Exchange).

---
+++ InterlockedExchange.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+namespace CoverityIssues
+{
+	public static class InterlockedExchange
+	{
+		public static void TestMethod()
+		{
+			s_object = new object();
+			TestMethod(s_object);
+		}
+
+		public static void TestMethod(object existing)
+		{
+			object old = Interlocked.Exchange(ref s_object, null);
+			if (old == existing)
+				Console.WriteLine(old.ToString());
+		}
+
+		static object s_object;
+	}
+}
