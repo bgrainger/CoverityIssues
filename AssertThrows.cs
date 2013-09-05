4571a7d301564fb4488bcca12e42a4172be9a52e
Bradley Grainger <bgrainger@gmail.com>
2013-09-04 21:02:19 -0700

    Repro FORWARD_NULL in test code.

---
+++ AssertThrows.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace CoverityIssues
+{
+	[TestFixture]
+	public class AssertThrows
+	{
+		[Test]
+		public void AssertThrowsArgumentNullException()
+		{
+			// Explicit null dereferenced (FORWARD_NULL)
+			// var_deref_model: Passing "null" to "ThrowArgumentNullException", which dereferences it.
+			Assert.Throws<ArgumentNullException>(() => ThrowArgumentNullException(null));
+		}
+
+		void ThrowArgumentNullException(object obj)
+		{
+			if (obj == null)
+				throw new ArgumentNullException("obj");
+		}
+	}
+}
