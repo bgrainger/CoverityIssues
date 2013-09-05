f6e030262a52f9988d4599483b321cfb26fdd770
Bradley Grainger <bgrainger@gmail.com>
2013-09-04 21:41:45 -0700

    Repro RESOURCE_LEAK with "yield break".

---
+++ YieldBreak.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace CoverityIssues
+{
+	public static class YieldBreak
+	{
+		public static IEnumerable<int> TestMethod()
+		{
+			// 1. new_resource: Created a new object of type System.IO.FileStream, which implements System.IDisposable.
+			// 2. var_assign: Assigning: "$locvar0" = resource returned from "new System.IO.FileStream("C:\\temp\\tmp.tmp", Create)".
+			using (new System.IO.FileStream("C:\\temp\\tmp.tmp", FileMode.Create))
+			{
+				// Resource leak (RESOURCE_LEAK)
+				// 3. leaked_resource: Returning without closing "$locvar0" leaks the resource that it refers to.
+				yield break;
+			}			
+		}
+	}
+}
