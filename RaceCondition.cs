fdcf776a1a5ba4a769c1ca21f816e5da7d1c9c50
Bradley Grainger <bgrainger@gmail.com>
2013-09-26 08:14:18 -0700

    Add code that should generate a data race issue.

---
+++ RaceCondition.cs
@@ -0,0 +1,24 @@
+﻿namespace CoverityIssues
+{
+	public class RaceCondition
+	{
+		public void SafeIncrement()
+		{
+			lock (m_lock)
+			{
+				m_counter++;
+				m_counter++;
+			}
+		}
+
+		public void UnsafeIncrement()
+		{
+			// expect a "Data race condition (GUARDED_BY_VIOLATION)" here, but don't get one
+			m_counter++;
+			m_counter++;
+		}
+
+		readonly object m_lock = new object();
+		int m_counter;
+	}
+}
